ec1d6d269c7eb49bd074db47d62071e0100bd9c5
Trizon <mohsenmacabangun11@gmail.com>
2026-06-06 18:28:46 +0800

    Fix implementation line formatting in build.gradle.kts

--- ZalithLauncher/build.gradle.kts
+++ ZalithLauncher/build.gradle.kts
@@ -264,6 +264,6 @@ dependencies {
     implementation("io.github.petterpx:floatingx:2.3.3")
     implementation("org.greenrobot:eventbus:3.3.1")
     
-    implementation("com.moandjiezana.toml:toml4j:0.7.2") {
+    implementation("com.moandjiezana.toml:toml4j:0.7.2")
         exclude(group = "com.google.code.gson", module = "gson")
 }
